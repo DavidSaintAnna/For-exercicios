e3010dcb943a5b4a0456943f1b2bc72c208c9069
David <david.henri.santana@gmail.com>
2022-06-20 10:27:55 -0300

    refactoring
    Made some changes improving some ideas.

--- script.js
+++ script.js
@@ -1,61 +1,125 @@
-let voltasGanhasPedro = 0;
-let voltasGanhasEdna = 0;
-let voltasGanhasJuca = 0;
+const btnTenLaps = document.querySelector(".btn-ten-laps");
+const btnSeventyLaps = document.querySelector(".btn-seventy-laps");
+const btnOneHundredSixtyLaps = document.querySelector(
+  ".btn-one-hundred-sixty-laps"
+);
 
-function run(voltas) {
-    let numeroVoltas = voltas;
-    for (let i = 0; i < numeroVoltas; i++) {
-        let velPedro = Math.round(Math.random() * (230 - 150) + 150);
-        let velJuca = Math.round(Math.random() * (260 - 120) + 120);
-        let velEdna = Math.round(Math.random() * (220 - 180) + 180);
-        let velocidadeFinalPedro = Math.round(velPedro - (0.03 * velPedro));
-        let velocidadeFinalJuca = Math.round(velJuca - (0.05 * velJuca));
-        let velocidadeFinalEdna = Math.round(velEdna - (0.01 * velEdna));
-        if (velocidadeFinalPedro > velocidadeFinalJuca && velocidadeFinalPedro > velocidadeFinalEdna) {
-            voltasGanhasPedro++
-        } else if (velocidadeFinalEdna > velocidadeFinalJuca) {
-            voltasGanhasEdna++
-        } else {
-            voltasGanhasJuca++
-        }
-    }
-    if (voltasGanhasPedro > voltasGanhasEdna && voltasGanhasPedro > voltasGanhasJuca) {
-        console.log("Pedro é o vencedor " + voltasGanhasPedro);
-        document.getElementById("options_and_result").innerHTML = " ";
-        document.getElementById("options_and_result").innerHTML = `
-        <p id="text">Resultado</p>
-        <p>Pedro é o vencedor! ${voltasGanhasPedro} voltas</p>
-        <button onclick="reset()">Reset</button>
-        `;
-    } else if (voltasGanhasEdna > voltasGanhasJuca) {
-        console.log("Edna é a vencedora" + voltasGanhasEdna);
-        document.getElementById("options_and_result").innerHTML = " ";
-        document.getElementById("options_and_result").innerHTML = `
-        <p id="text">Resultado</p>
-        <p>Edna é a vencedora! ${voltasGanhasEdna} voltas </p>
-        <button onclick="reset()">Reset</button>
-        `
+let numberOfLapsWonByPedro = 0;
+let numberOfLapsWonByEdna = 0;
+let numberOfLapsWonByJuca = 0;
 
-    } else {
-        console.log("Juca é o vencedor" + voltasGanhasJuca);
-        document.getElementById("options_and_result").innerHTML = " ";
-        document.getElementById("options_and_result").innerHTML = `
-        <p id="text">Resultado</p>
-        <p>Juca é o vencedor! ${voltasGanhasJuca} voltas</p>
-        <button onclick="reset()">Reset</button>
-        `
-    }
+const minVelocityOfJucaCar = 120;
+const maxVelocityOfJucaCar = 260;
+const skidPercentualOfJucaCar = 0.05;
+
+const minVelocityOfEdnaCar = 180;
+const maxVelocityOfEdnaCar = 220;
+const skidPercentualOfEdnaCar = 0.01;
+
+const minVelocityOfPedroCar = 150;
+const maxVelocityOfPedroCar = 230;
+const skidPercentualOfPedroCar = 0.03;
+
+function velocityCar(maxVelocity, minVelocity) {
+  return Math.random() * (maxVelocity - minVelocity) + minVelocity;
 }
 
-function reset() {
-    voltasGanhasPedro = 0;
-    voltasGanhasEdna = 0;
-    voltasGanhasJuca = 0;
+function race(laps) {
+  for (let counter = 0; counter < laps; counter++) {
+    const randomVelocityBasedOnPedroCar = velocityCar(
+      maxVelocityOfPedroCar,
+      minVelocityOfPedroCar
+    );
+    const randomVelocityBasedOnJucaCar = velocityCar(
+      maxVelocityOfJucaCar,
+      minVelocityOfJucaCar
+    );
+    const randomVelocityBasedOnEdnaCar = velocityCar(
+      maxVelocityOfEdnaCar,
+      minVelocityOfEdnaCar
+    );
+
+    const roundedVelocityOfPedroCar = Math.round(randomVelocityBasedOnPedroCar);
+    const roundedVelocityOfJucaCar = Math.round(randomVelocityBasedOnJucaCar);
+    const roundedVelocityOfEdnaCar = Math.round(randomVelocityBasedOnEdnaCar);
+
+    const finalVelocityPedroCar = Math.round(
+      roundedVelocityOfPedroCar -
+        roundedVelocityOfPedroCar * skidPercentualOfPedroCar
+    );
+    const finalVelocityJucaCar = Math.round(
+      roundedVelocityOfJucaCar -
+        roundedVelocityOfJucaCar * skidPercentualOfJucaCar
+    );
+    const finalVelocityEdnaCar = Math.round(
+      roundedVelocityOfEdnaCar -
+        roundedVelocityOfEdnaCar * skidPercentualOfEdnaCar
+    );
+
+    const pedroWonTheLap =
+      finalVelocityPedroCar > finalVelocityJucaCar &&
+      finalVelocityPedroCar > finalVelocityEdnaCar;
+    const ednaWonTheLap =
+      finalVelocityEdnaCar > finalVelocityJucaCar &&
+      finalVelocityEdnaCar > finalVelocityPedroCar;
+    if (
+      finalVelocityEdnaCar !== finalVelocityJucaCar &&
+      finalVelocityJucaCar !== finalVelocityPedroCar
+    ) {
+      if (pedroWonTheLap) {
+        numberOfLapsWonByPedro++;
+      } else if (ednaWonTheLap) {
+        numberOfLapsWonByEdna++;
+      } else {
+        numberOfLapsWonByJuca++;
+      }
+    }
+  }
+  if (
+    numberOfLapsWonByPedro > numberOfLapsWonByEdna &&
+    numberOfLapsWonByPedro > numberOfLapsWonByJuca
+  ) {
+    console.log("Pedro é o vencedor " + numberOfLapsWonByPedro);
+    console.log(numberOfLapsWonByJuca);
+    console.log(numberOfLapsWonByEdna);
+    console.log(numberOfLapsWonByPedro);
     document.getElementById("options_and_result").innerHTML = " ";
     document.getElementById("options_and_result").innerHTML = `
-    <p id="text">Qual tipo de corrida você quer ?</p>
-    <button onclick="run(10)">Corrida rápida: 10 voltas</button>
-    <button onclick="run(70)">Grande Prêmio: 70 voltas</button>
-    <button onclick="run(160)">Enduro: 160 voltas</button>
-    `
+    <p id="text">Resultado</p>
+    <p>Pedro é o vencedor! ${numberOfLapsWonByPedro} voltas</p>
+    <button onclick="location.reload()">Reload</button>
+    `;
+  } else if (numberOfLapsWonByEdna > numberOfLapsWonByJuca) {
+    console.log("Edna é a vencedora" + numberOfLapsWonByEdna);
+    console.log(numberOfLapsWonByJuca);
+    console.log(numberOfLapsWonByEdna);
+    console.log(numberOfLapsWonByPedro);
+    document.getElementById("options_and_result").innerHTML = " ";
+    document.getElementById("options_and_result").innerHTML = `
+    <p id="text">Resultado</p>
+    <p>Edna é a vencedora! ${numberOfLapsWonByEdna} voltas </p>
+    <button onclick="location.reload()">Reload</button>
+    `;
+  } else {
+    console.log("Juca é o vencedor" + numberOfLapsWonByJuca);
+    console.log(numberOfLapsWonByJuca);
+    console.log(numberOfLapsWonByEdna);
+    console.log(numberOfLapsWonByPedro);
+    document.getElementById("options_and_result").innerHTML = " ";
+    document.getElementById("options_and_result").innerHTML = `
+    <p id="text">Resultado</p>
+    <p>Juca é o vencedor! ${numberOfLapsWonByJuca} voltas</p>
+    <button onclick="location.reload()">Reload</button>
+    `;
+  }
 }
+
+btnTenLaps.addEventListener("click", () => {
+  race(10);
+});
+btnSeventyLaps.addEventListener("click", () => {
+  race(70);
+});
+btnOneHundredSixtyLaps.addEventListener("click", () => {
+  race(160);
+});
